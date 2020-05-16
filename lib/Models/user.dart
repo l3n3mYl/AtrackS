c2728fd6ef2d513672af7cfbbddf997da55778e2
asavkina <asavkina@coventry.ac.uk>
2020-05-16 14:01:02 +0100

    Registration fields are now accepting and sending information to the user model

---
+++ lib/Models/user.dart
@@ -0,0 +1,54 @@
+class User{
+  String username, password, email, weight, height, gender;
+
+  get name {
+    return this.username;
+  }
+
+  set name (String username){
+    this.username = username;
+  }
+
+  get pass {
+    return this.password;
+  }
+
+  set pass (String password){
+    this.password = password;
+  }
+
+  get mail {
+    return this.email;
+  }
+
+  set mail (String email) {
+    this.email = email;
+  }
+
+  get w {
+    return this.weight;
+  }
+
+  set w (String weight){
+    this.weight = weight;
+  }
+
+  get h {
+    return this.height;
+  }
+
+  set h (String height){
+    this.height = height;
+  }
+
+  get g {
+    return this.gender;
+  }
+
+  set g (String gender) {
+    this.gender = gender;
+  }
+
+  User({this.username, this.password, this.email, this.weight, this.height, this.gender});
+
+}
