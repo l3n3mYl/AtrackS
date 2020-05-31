c916da4175b2a56ad05aba7aa23d23b17e7047c6
asavkina <asavkina@coventry.ac.uk>
2020-05-31 11:23:22 +0100

    Added and adjusted a progress showing graph in the steps screen

--- lib/Screens/ProgressScreen/walking_screen.dart
+++ lib/Screens/ProgressScreen/walking_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:com/Database/Services/db_management.dart';
 import 'package:com/Design/colours.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pedometer/pedometer.dart';
@@ -22,6 +23,8 @@ class _WalkingScreenState extends State<WalkingScreen>{
   Pedometer _pedometer;
   StreamSubscription<int> _subscription;
   DatabaseManagement _management;
+//  List<String> weeklyProgress = new List<String>();
+  List<FlSpot> weeklyStepList = new List<FlSpot>();
   String stepCountVal = '?';
   String stepGoal = '0';
   String totalSteps = '0';
@@ -31,12 +34,27 @@ class _WalkingScreenState extends State<WalkingScreen>{
   @override
   void initState() {
     super.initState();
+    getWeeklyStepProgress();
     initPlatformState();
     getTotalSteps();
     getStepGoal();
   }
+  
+  void getWeeklyStepProgress() async {
+    _management = DatabaseManagement(widget.user);
+    await _management.getSingleFieldInfo('exercise_weekly_progress', 'Cycling')
+        .then((result) {
+          List<String> resList = result.split(", ");
+          setState(() {
+            for(var i = 0; i < resList.length; ++i){
+              weeklyStepList.add(FlSpot(i.toDouble(), double.parse(resList[i])));
+              if(i == resList.length - 1) weeklyStepList.add(FlSpot(6.9, double.parse(resList[i])));
+            }
+          });
+    });
+  }
 
-  Future<void> initPlatformState() async {
+  void initPlatformState() async {
     startListening();
   }
 
@@ -139,12 +157,318 @@ class _WalkingScreenState extends State<WalkingScreen>{
             Container(
               child: RaisedButton(
                 child: Text('Stop'),
-                onPressed: () => _onDone(),
+                onPressed: () => print(DateTime.now().weekday),
+              ),
+            ),
+            Container(
+              child: LineChartSample2(
+                weeklyStepList.isEmpty
+                    ? [FlSpot(0.0, 0.0), FlSpot(2.0, 0.0), FlSpot(5.0, 0.0)]
+                    : weeklyStepList,
+                [FlSpot(0,0)]
               ),
             )
           ],
         )
       ),
+    );
+  }
+}
+
+class LineChartSample2 extends StatefulWidget {
+
+  LineChartSample2(this._firstChart, this._secondChart);
+
+  final List<FlSpot> _firstChart;
+  final List<FlSpot> _secondChart;
+
+  @override
+  _LineChartSample2State createState() => _LineChartSample2State();
+}
+
+class _LineChartSample2State extends State<LineChartSample2> {
+
+//  final List<Color> gradientColors = [
+//    const Color(0xff23b6e6),
+//    const Color(0xff02d39a),
+//  ];
+
+  bool showAvg = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final List<FlSpot> firstDiag = widget._firstChart;
+    final List<FlSpot> secondDiag = widget._secondChart;
+
+    return Stack(
+      children: <Widget>[
+        AspectRatio(
+          aspectRatio: 1.70,
+          child: Container(
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(18),
+                ),
+                color: Colors.transparent),
+            child: Padding(
+              padding: const EdgeInsets.only(right: 18.0, left: 12.0, top: 24, bottom: 12),
+              child: LineChart(
+                showAvg ? avgData() : mainData(firstDiag),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 60,
+          height: 34,
+          child: FlatButton(
+            onPressed: () {
+              setState(() {
+                showAvg = !showAvg;
+              });
+            },
+            child: Text(
+              'avg',
+              style: TextStyle(
+                  fontSize: 12, color: showAvg ? Colors.white.withOpacity(0.5) : Colors.white),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  LineChartData mainData(List<FlSpot> firstList) {
+    final List<Color> gradientColors = [
+      const Color(0xff23b6e6),
+      const Color(0xff02d39a),
+    ];
+    return LineChartData(
+      gridData: FlGridData(
+        show: true,
+        drawVerticalLine: false,
+        drawHorizontalLine: true,
+        getDrawingHorizontalLine: (value) {
+          return FlLine(
+            color: Colors.black, //horizontal lines
+            strokeWidth: 1,
+          );
+        },
+//        getDrawingVerticalLine: (value) {
+//          return FlLine(
+//            color: Colors.black, //vertical lines
+//            strokeWidth: 1,
+//          );
+//        },
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 22,
+          textStyle:
+          const TextStyle(color: Colors.red, fontWeight: FontWeight.bold, fontSize: 16),
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 0:
+                return 'M';
+              case 1:
+                return 'T';
+              case 2:
+                return 'W';
+              case 3:
+                return 'T';
+              case 4:
+                return 'F';
+              case 5:
+                return 'S';
+              case 6:
+                return 'S';
+            }
+            return '';
+          },
+          margin: 8,
+        ),
+        leftTitles: SideTitles(
+          showTitles: true,
+          textStyle: const TextStyle(
+            color: Color(0xff67727d),
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 1:
+                return '1k';
+              case 3:
+                return '3k';
+              case 5:
+                return '5k';
+              case 7:
+                return '7k';
+              case 9:
+                return '9k';
+            }
+            return '';
+          },
+          reservedSize: 28,
+          margin: 12,
+        ),
+      ),
+      borderData:
+      FlBorderData(show: true, border: Border.all(color: Colors.black, width: 1)),
+      minX: 0,
+      maxX: 7,
+      minY: 0,
+      maxY: 10,
+      lineBarsData: [
+        LineChartBarData(
+          spots: firstList,
+          isCurved: true,
+          colors: gradientColors,
+          barWidth: 5,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            colors: gradientColors.map((color) => color.withOpacity(0.3)).toList(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  LineChartData avgData() {
+    final List<Color> gradientColors = [
+      const Color(0xff23b6e6),
+      const Color(0xff02d39a),
+    ];
+    return LineChartData(
+      lineTouchData: LineTouchData(enabled: false),
+      gridData: FlGridData(
+        show: true,
+        drawHorizontalLine: true,
+//        drawVerticalLine: true,
+//        getDrawingVerticalLine: (value) {
+//          return FlLine(
+//            color: Colors.black,
+//            strokeWidth: 1,
+//          );
+//        },
+        getDrawingHorizontalLine: (value) {
+          return FlLine(
+            color: Colors.black,
+            strokeWidth: 1,
+          );
+        },
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 22,
+          textStyle:
+          const TextStyle(color: Color(0xff68737d), fontWeight: FontWeight.bold, fontSize: 16),
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 1:
+                  if(DateTime.now().month - 4 < 1)
+                    {
+                      int temp = DateTime.now().month - 4;
+                      return '${12 + temp}';
+                    }
+                  else return '${DateTime.now().month - 4}';
+                  break;
+              case 3:
+                  if(DateTime.now().month - 3 < 1)
+                  {
+                    int temp = DateTime.now().month - 3;
+                    return '${12 + temp}';
+                  }
+                  else return '${DateTime.now().month - 3}';
+                  break;
+              case 5:
+                if(DateTime.now().month - 2 < 1)
+                {
+                  int temp = DateTime.now().month - 2;
+                  return '${12 + temp}';
+                }
+                else return '${DateTime.now().month - 2}';
+                break;
+              case 7:
+                if(DateTime.now().month - 1 < 1)
+                {
+                  int temp = DateTime.now().month - 1;
+                  return '${12 + temp}';
+                }
+                else return '${DateTime.now().month - 1}';
+                break;
+              case 9:
+                return '${DateTime.now().month}';
+            }
+            return '';
+          },
+          margin: 8,
+        ),
+        leftTitles: SideTitles(
+          showTitles: true,
+          textStyle: const TextStyle(
+            color: Color(0xff67727d),
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+          getTitles: (value) {
+            switch (value.toInt()) {
+              case 1:
+                return '1k';
+              case 5:
+                return '5k';
+              case 10:
+                return '10k';
+            }
+            return '';
+          },
+          reservedSize: 28,
+          margin: 12,
+        ),
+      ),
+      borderData:
+      FlBorderData(show: true, border: Border.all(color: Colors.black, width: 1)),
+      minX: 0,
+      maxX: 10,
+      minY: 0,
+      maxY: 11,
+      lineBarsData: [
+        LineChartBarData(
+          spots: [
+            FlSpot(0, 0.2),
+            FlSpot(1, 6),
+            FlSpot(3, 3),
+            FlSpot(5, 10),
+            FlSpot(7, 10),
+            FlSpot(9, 4),
+            FlSpot(10, 4)
+          ],
+          isCurved: true,
+          colors: [
+            ColorTween(begin: gradientColors[0], end: gradientColors[1]).lerp(0.5),
+            ColorTween(begin: gradientColors[0], end: gradientColors[1]).lerp(0.2),
+          ],
+          barWidth: 5,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(show: true, colors: [
+            ColorTween(begin: gradientColors[0], end: gradientColors[1]).lerp(0.5).withOpacity(0.1),
+            ColorTween(begin: gradientColors[0], end: gradientColors[1]).lerp(0.2).withOpacity(0.1),
+          ]),
+        ),
+      ],
     );
   }
 }
