eee670491b76b320bdfb535faeb12918267d51a6
asavkina <asavkina@coventry.ac.uk>
2020-06-02 11:43:44 +0100

    Started implementing new screen, adding more dynamic elements for a better reusability

--- lib/Screens/ProgressScreen/push_ups_screen.dart
+++ lib/Screens/ProgressScreen/push_ups_screen.dart
@@ -1,98 +1,128 @@
 import 'dart:async';
 import 'package:com/Database/Services/db_management.dart';
+import 'package:com/Design/colours.dart';
+import 'package:com/UiComponents/background_triangle_clipper.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:pedometer/pedometer.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class PushUpsScreen extends StatefulWidget {
-
   final FirebaseUser user;
+  final String appBarTitile;
+  final Color accentColor;
+  final String icon;
 
-  PushUpsScreen(this.user);
+  PushUpsScreen({this.user, this.accentColor, this.icon, this.appBarTitile});
 
   @override
   _PushUpsScreenState createState() => _PushUpsScreenState();
 }
 
-class _PushUpsScreenState extends State<PushUpsScreen>{
-
-  Pedometer _pedometer;
-  DatabaseManagement _management;
-  StreamSubscription<int> _subscription;
-  String stepCountVal = '?';
-  int resetValue = 0;
-  bool reset = true;
-
+class _PushUpsScreenState extends State<PushUpsScreen> {
   @override
   void initState() {
     super.initState();
-    initPlatformState();
   }
-
-  Future<void> initPlatformState() async {
-    startListening();
-  }
-
-  void startListening() {
-    _pedometer = new Pedometer();
-    _subscription = _pedometer.pedometerStream.listen(_onData, onError: _onError,
-        onDone: _onDone, cancelOnError: true);
-  }
-
-  void _onData(int newValue) async {
-    if(reset) {
-      resetValue += newValue;
-      reset = false;
-    }
-    setState(() {
-      stepCountVal = "${newValue - resetValue}";
-    });
-  }
-
-  void _onDone() async {
-    reset = true;
-    _management = new DatabaseManagement(widget.user);
-    String oldDbValue = await _management.getSingleFieldInfo('exercises', 'Steps');
-    String sumOfValues = (int.parse(oldDbValue) + int.parse(stepCountVal)).toString();
-    await _management.updateSingleField('exercises', 'Steps', sumOfValues);
-  }
-
-  void _onError(error) => print('Error: $error');
 
   @override
   void dispose() {
     super.dispose();
-    _subscription.cancel();
-    _onDone();
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.red,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                child: Text('Steps: $stepCountVal', style: TextStyle(
-                    color: Colors.black
-                ),),
-              ),
-              Container(
-                child: RaisedButton(
-                  child: Text('Stop'),
-                  onPressed: () => _onDone(),
+      appBar: AppBar(
+        backgroundColor: mainColor,
+        title: Text(
+          widget.appBarTitile,
+          style: TextStyle(fontFamily: 'PTSerif'),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            FontAwesomeIcons.arrowLeft,
+          ),
+          color: Colors.white,
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: mainColor,
+          ),
+          BackgroundTriangle(),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: AspectRatio(
+              aspectRatio: 18 / 9,
+              child: Opacity(
+                opacity: 0.2,
+                child: Image(
+                  image: AssetImage('images/main_pattern.jpg'),
+                  fit: BoxFit.cover,
                 ),
-              )
-            ],
-          )
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: Colors.transparent,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 13.0),
+                  width: 256,
+                  height: 256,
+                  child: CircularPercentIndicator(
+                    backgroundColor: mainColor,
+                    radius: 250.0,
+                    progressColor: widget.accentColor,
+                    lineWidth: 3.0,
+                    animationDuration: 20,
+//    percent: stepGoal == null && totalSteps == null
+//    ? 0.01
+//        : double.parse(totalSteps) >= double.parse(stepGoal)
+//    ? 1.0
+//        : (double.parse(totalSteps) *
+//    100 /
+//    double.parse(stepGoal)) /
+//    100,
+                    percent: 0.5,
+                    //TODO: Change
+                    animation: true,
+                    center: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Image.asset(
+                          widget.icon,
+                          width: 150,
+                          height: 150,
+                          color: widget.accentColor,
+                        ),
+//    Text(stepGoal == null && totalSteps == null
+//    ? '0 - 0%'
+//        : '$totalSteps - ${((int.parse(totalSteps) * 100 / int.parse(stepGoal))).toStringAsFixed(0)}%',
+//    style: TextStyle(
+//    color: Colors.white54
+//    ),
+//    )//TODO: change
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
-
 }
