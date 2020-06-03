8ce69cbd11eef997b56a2732428d02dba48c11a6
asavkina <asavkina@coventry.ac.uk>
2020-06-03 10:53:26 +0100

    Adjusted the steps update functionality

--- lib/Database/Services/db_management.dart
+++ lib/Database/Services/db_management.dart
@@ -2,31 +2,46 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class DatabaseManagement {
-
   DatabaseManagement(this._user);
 
   final FirebaseUser _user;
 
   final Firestore _reference = Firestore.instance;
 
+  Future updateWeeklyProgress(int day, String amount, String collection, String field) async {
+    try{
+      DocumentSnapshot snapshot =
+          await _reference.collection(collection).document(_user.uid).get();
+      List<String> list = snapshot[field].split(", ");
+      list[day - 1] = amount;
+      final String data = list.reduce((value, element) => value + ', ' + element);
+      final Map<String, String> map = {field : data};
+      await _reference.collection(collection).document(_user.uid).updateData(map);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
   Future<Map<String, dynamic>> retrieveExerciseInfoByUid() async {
     Map<String, dynamic> exerciseInfo = new Map();
-    try{
+    try {
       await _reference.collection('exercises').getDocuments().then((value) {
-        value.documents.forEach((doc){
+        value.documents.forEach((doc) {
           exerciseInfo = doc.data;
         });
       });
-    return exerciseInfo;
-    } catch(e) {
+      return exerciseInfo;
+    } catch (e) {
       print(e.toString());
       return null;
     }
   }
 
   Future<String> getSingleFieldInfo(String collection, String field) async {
-    try{
-      DocumentSnapshot snapshot = await _reference.collection(collection).document(_user.uid).get();
+    try {
+      DocumentSnapshot snapshot =
+          await _reference.collection(collection).document(_user.uid).get();
       return snapshot[field];
     } catch (e) {
       print(e.toString());
@@ -34,16 +49,20 @@ class DatabaseManagement {
     }
   }
 
-  Future updateSingleField(String collection, String field, String count) async {
-    Map<String, String> data = {field : count};
-    try{
-      await _reference.collection(collection).document(_user.uid).updateData(data);
+  Future updateSingleField(
+      String collection, String field, String count) async {
+    Map<String, String> data = {field: count};
+    try {
+      await _reference
+          .collection(collection)
+          .document(_user.uid)
+          .updateData(data);
     } catch (e) {
       print(e.toString());
     }
   }
 
-  //TODO: DON'T TOUCH
+//TODO: DON'T TOUCH
 //  Future deleteFirebaseDocs() async {
 //    _reference.collection('exercises').getDocuments().then((snapshot) {
 //      for(DocumentSnapshot ds in snapshot.documents){
